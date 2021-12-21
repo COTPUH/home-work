d8078b67d4ddedad371644ee618b729673f185e8
COTPUH <93445918+COTPUH@users.noreply.github.com>
2021-12-21 20:54:35 +0300

    Add files via upload

---
+++ dyplom/script/script.js
@@ -0,0 +1,20 @@
+$('.about-items>div').on('click', function(){
+    $(this).children('.about-icon').toggleClass('active');
+    $(this).parent('.about-items').toggleClass('toggleBg');
+    $(this).toggleClass('toggleColor');
+    $(this).next('p').slideToggle(300).toggleClass('toggleColor')
+})
+
+$('.items-selector>div').on('click',function() {
+ $(this).next('p').slideToggle(300);
+ $(this).next('.items__hr').toggleClass('toggleBg');
+ $(this).children('.selector__pre-title').toggleClass('items-toggleColor');
+
+})
+
+
+
+
+
+
+
